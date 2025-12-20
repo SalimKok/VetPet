f1b5931321952843fb93b5632ddcb11ca1c4ad5c
Salim kök <155094272+SalimKok@users.noreply.github.com>
2025-12-21 01:36:41 +0300

    Mask database URI in init_db function
    Updated database URI to hide sensitive information.

--- database.py
+++ database.py
@@ -2,8 +2,9 @@ from extensions import db
 from flask import Flask
 
 def init_db(app: Flask):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:sk6137!@localhost:5432/petvet'
+    app.config['SQLALCHEMY_DATABASE_URI'] = '************************'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     with app.app_context():
         db.create_all()
+
